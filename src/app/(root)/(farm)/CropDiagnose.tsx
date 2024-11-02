73fbac93585d4e8a6d75186d3fa4da3bd21bd4ad
Ganesh Kasture <ganeshkasture9595@gmail.com>
2024-11-02 20:34:23 +0530

    test

---
+++ src/app/(root)/(farm)/CropDiagnose.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CropDiagnose = () => {
+  return (
+    <View>
+      <Text>CropDiagnose</Text>
+    </View>
+  )
+}
+
+export default CropDiagnose
